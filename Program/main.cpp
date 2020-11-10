c3c12939740472b4fcaa6b40fcec17e1be3c9379
SergiyStepanchuk <serhii.stepanchuk.itis.2020@lpnu.ua>
2020-11-10 22:58:05 +0200

    init

---
+++ Program/main.cpp
@@ -0,0 +1,21 @@
+// Lab_5_5
+#include <iostream>
+using namespace std;
+
+double a(const int& n, int level, int &depth) {
+	if (level > depth)
+		depth = level;
+	cout << "L = " << level << endl;
+	return n > 1 ?
+		(2 * a(n - 1, level + 1, depth) + 8 / pow(a(n - 1, level + 1, depth), 2)) / 3
+		: 1;
+}
+
+void main() {
+	int deep = 0;
+	double a_;
+	cout << "a start "; cin >> a_;
+
+	cout << a(a_, 1, deep) << endl;
+	cout << deep << endl;
+} 
---
+++ UnitTest/UnitTest.cpp
@@ -0,0 +1,20 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Program/main.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest
+{
+	TEST_CLASS(UnitTest)
+	{
+	public:
+		
+		TEST_METHOD(met_a)
+		{
+			int d;
+			a(4, 1, d);
+			Assert::AreEqual(d, 4, L"Incorrect method a!");
+		}
+	};
+}
