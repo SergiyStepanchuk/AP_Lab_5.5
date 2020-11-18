dd0cb9c969ab9d01ae838bd2e214cd26b0c4602b
SergiyStepanchuk <prochliste@gmail.com>
2020-11-18 20:54:03 +0200

    dovedeno

--- Program/main.cpp
+++ Program/main.cpp
@@ -5,7 +5,7 @@ using namespace std;
 double a(const int& n, int level, int &depth) {
 	if (level > depth)
 		depth = level;
-	cout << "L = " << level << endl;
+	//cout << "L = " << level << endl;
 	return n > 1 ?
 		(2 * a(n - 1, level + 1, depth) + 8 / pow(a(n - 1, level + 1, depth), 2)) / 3
 		: 1;
@@ -18,4 +18,11 @@ void main() {
 
 	cout << a(a_, 1, deep) << endl;
 	cout << deep << endl;
+
+	// доведення
+
+	for (size_t i = 1; i < 500; i++)
+	{
+		cout << i << " " << a(i, 1, deep);
+	}
 } 
